84a9b52669a093a7e9a14ba6c5261b170f37098e
unknown <x444428@yahoo.com>
2015-06-26 18:20:30 -0600

    Add sorting methods

---
+++ app/src/main/java/com/msu_software_factory/thesortingguide/Sorting.java
@@ -0,0 +1,58 @@
+package com.msu_software_factory.thesortingguide;
+
+import java.util.Random;
+
+public class Sorting {
+    static Random randomgenerator = new Random();
+    
+    public static int[] randlist(int size){
+        int[] newlist = new int[size];
+        for (int i = 0; i < newlist.length; i++) {
+            int randomInt = randomgenerator.nextInt(50);
+            newlist[i] = randomInt;
+        }
+        return newlist;
+    }
+    public static void printlist(int[] list){
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void bubblesort(int[] unsorted){
+        for (int i = 1; i < unsorted.length; i++) {
+            for (int j = 0; j < unsorted.length -i; j++) {
+                if (unsorted[j] > unsorted[j+1]){
+                    int temp = unsorted[j];
+                    unsorted[j] = unsorted[j+1];
+                    unsorted[j+1] = temp;
+                }
+            }
+        }
+    }
+    public static void selectionsort(int[] unsorted){
+        for (int i = 0; i < unsorted.length -2; i++) {
+            int min = i;
+            for (int j = i +1; j <= unsorted.length - 1; j++) {
+                if (unsorted[j] < unsorted[min]){
+                    min = j;
+                }
+            }
+            if (min != i){
+                int temp = unsorted[min];
+                unsorted[min] = unsorted[i];
+                unsorted[i] = temp;
+            }
+        }
+    }
+    public static void insertionsort(int[] unsorted){
+        for (int i = 1; i < unsorted.length; i++) {
+            int temp = unsorted[i];
+            int j;
+            for (j = i-1; j >= 0 && temp < unsorted[j]; j--) {
+                unsorted[j+1] = unsorted[j];
+            }
+            unsorted[j+1] = temp;
+        }
+    }
+}
