a1c50a0349379b6597e1b5f4a2a1a6ca4d0b7a13
unknown <x444428@yahoo.com>
2015-07-05 17:40:06 -0600

    created settings page

--- app/src/main/java/com/msu_software_factory/thesortingguide/MainActivity.java
+++ app/src/main/java/com/msu_software_factory/thesortingguide/MainActivity.java
@@ -67,8 +67,10 @@ public class MainActivity extends ActionBarActivity
     public Fragment getSelectedTab(int pos){
         if(pos == 1){
             return about;
+        }else if (pos == 5) {
+            return new SettingsPage();
         }else{
-            return PlaceholderFragment.newInstance(pos);
+                return PlaceholderFragment.newInstance(pos);
         }
 
     }
@@ -87,6 +89,9 @@ public class MainActivity extends ActionBarActivity
                 break;
             case 4:
                 mTitle = getString(R.string.title_section3);
+                break;
+            case 5:
+                mTitle = "Settings";
                 break;
         }
     }
@@ -222,12 +227,18 @@ public class MainActivity extends ActionBarActivity
         return arr;
     }
 
-    public static class settingsPage extends ActionBarActivity{
+    public static class SettingsPage extends Fragment{
         @Override
-        public void onCreate(Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.fragment_settings_page);
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_settings_page, container, false);
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            ((MainActivity) activity).onSectionAttached(5);
+
         }
     }
-
 }
